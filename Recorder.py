96086bae2894e4e7b0b4451e3e2c3782ca9752fb
Mostafa <dahshury@gmail.com>
2024-01-11 19:58:31 +0300

    Converted to OOP, added VAD (Voice Activity Detection), Added Faster-Whisper for CPU

--- Recorder.py
+++ Recorder.py
@@ -7,50 +7,46 @@ import threading
 
 
 class Recorder():
-    #Defines sound properties like frequency and channels
     def __init__(self, chunk=1024, channels=2, rate=44100):
         self.CHUNK = chunk
         self.FORMAT = pyaudio.paInt16
         self.CHANNELS = channels
         self.RATE = rate
-        self._running = True
+        self._running = threading.Event()
         self._frames = []
+        self.data = None
+        self.verbose = False
 
-    #Start recording sound
     def start(self):
-        threading._start_new_thread(self.__recording, ())
+        self._running.set()
+        threading.Thread(target=self.__recording).start()
 
     def __recording(self):
-        #Set running to True and reset previously recorded frames
-        self._running = True
         self._frames = []
-        #Create pyaudio instance
         p = pyaudio.PyAudio()
-        #Open stream
-        stream = p.open(format=self.FORMAT,
-                        channels=self.CHANNELS,
-                        rate=self.RATE,
-                        input=True,
-                        frames_per_buffer=self.CHUNK)
+        try:
+            stream = p.open(format=self.FORMAT,
+                            channels=self.CHANNELS,
+                            rate=self.RATE,
+                            input=True,
+                            frames_per_buffer=self.CHUNK)
+        except Exception as e:
+            raise OSError("Invalid configuration", e)
         
-        # To stop the streaming, new thread has to set self._running to false
-        # append frames array while recording
-        while(self._running):
-            data = stream.read(self.CHUNK)
-            self._frames.append(data)
+        while self._running.is_set():
+            self.data = stream.read(self.CHUNK)
+            self._frames.append(self.data)
 
-        # Interrupted, stop stream and close it. Terinate pyaudio process.
         stream.stop_stream()
         stream.close()
         p.terminate()
 
-    # Sets boolean to false. New thread needed.
     def stop(self):
-        self._running = False
+        self._running.clear()
 
-    #Save file to filename location as a wavefront file.
     def save(self, filename):
-        print("Saving")
+        if self.verbose:
+            print("Saving")
         p = pyaudio.PyAudio()
         if not filename.endswith(".wav"):
             filename = filename + ".wav"
@@ -60,21 +56,18 @@ class Recorder():
         wf.setframerate(self.RATE)
         wf.writeframes(b''.join(self._frames))
         wf.close()
-        print("Saved")
+        if self.verbose:
+            print("Saved")
 
-    # Delete a file
     @staticmethod
     def delete(filename):
         os.remove(filename)
 
-    # Convert wav to mp3 with same name using ffmpeg.exe
     @staticmethod
     def wavTomp3(wav):
         mp3 = wav[:-3] + "mp3"
-        # Remove file if existent
         if os.path.isfile(mp3):
             Recorder.delete(mp3)
-        # Call CMD command
         subprocess.call('ffmpeg -i "'+wav+'" "'+mp3+'"')
 
 
@@ -82,12 +75,20 @@ if __name__ == "__main__":
     rec = Recorder()
     print("Start recording")
     rec.start()
+    
+    # Allow some time for recording (adjust as needed)
     time.sleep(5)
+    
     print("Stop recording")
     rec.stop()
+    
+    # Wait for the recording thread to finish
+    rec_thread = threading.Thread(target=rec.start)
+    rec_thread.join()
+
     print("Saving")
     rec.save("test.wav")
     print("Converting wav to mp3")
     Recorder.wavTomp3("test.wav")
-    print("deleting wav")
+    print("Deleting wav")
     Recorder.delete("test.wav")
--- listener.py
+++ listener.py
@@ -1,112 +1,132 @@
-import joblib
+import numpy as np
+import wave
+import pyperclip
+import torch
+import threading
+from utils import logger
+from utils.utils import has_speech, get_model
 from keyboard import KEY_DOWN, KEY_UP, hook_key, wait
 from pynput.keyboard import Key, Controller
 from Recorder import Recorder
-import wave
-import time
-import pyaudio
-import pyperclip
-import os
-import torch
-from transformers import pipeline
-from transformers.utils import is_flash_attn_2_available
 
-# Directory to store cached data
-CACHE_DIR = "cache"
+# INT16_MAX_ABS_VALUE = 32768.0
+MODEL_SIZES = ['tiny.en', 'tiny', 'base.en', 'base', 'small.en', 'small', 'medium.en', 'medium', 'large', 'large-v2', 'large-v3']
+model_types = ['Faster-Whisper', 'Insanely-Fast-Whisper']
 
-# Ensure the cache directory exists
-os.makedirs(CACHE_DIR, exist_ok=True)
+class AudioToTextRecorder:
+    def __init__(self,
+                    hook_key: str = 'right ctrl',
+                    quit_key: str = 'ctrl+q',
+                    model_type: str = "Faster-Whisper",
+                    model_size: str = "small.en",
+                    channels: int=1,
+                    rate: int =16000,
+                    ):
+        
+        self.hook_key = hook_key
+        self.quit_key = quit_key
+        self.cuda = torch.cuda.is_available()
+        self.model_type = model_type
+        if self.model_type != "Faster-Whisper":
+            self.model_type = "Insanely-Fast-Whisper" if self.cuda else "Faster-Whisper"
+        self.channels = channels
+        self.model_size=model_size
+        self.rate = rate
+        self.rec = Recorder(channels=channels, rate=rate)
+        self.recording = False #! Remove after activating rec events
+        self.keyboard = Controller()
+        self.start_recording_event = threading.Event()
+        self.stop_recording_event = threading.Event()
+        
+        
+    def capture_keys(self):
+        # Set up key bindings
+        hook_key(self.hook_key, self.transcribe_recording)
+        wait(self.quit_key)
 
-# pipe = pipeline(
-#         "automatic-speech-recognition",
-#         model="openai/whisper-large-v3",
-#         torch_dtype=torch.float16,
-#         device="cuda:0" if torch.cuda.is_available() else "cpu",
-#         model_kwargs={"attn_implementation": "flash_attention_2"} if is_flash_attn_2_available() else {"attn_implementation": "sdpa"},
-#     )
-    
-# Try loading the pipeline from cache
-print("Loading...")
-pipe_cache_file = os.path.join(CACHE_DIR, "pipeline_cache.joblib")
+    def paste_transcription(self,transcript_text):
+        if self.model_type == "Insanely-Fast-Whisper":
+            transcript_text = transcript_text['text'].replace("New paragraph.", "\n\n")
+            logger.info(transcript_text['text'])
+        else:
+            transcript_text = transcript_text.replace("New paragraph.", "\n\n")
+            logger.info(transcript_text)
+            
+        transcript_text = transcript_text.strip()
+        pyperclip.copy(transcript_text)
+        self.keyboard.press(Key.ctrl)
+        self.keyboard.press('v')
+        self.keyboard.release('v')
+        self.keyboard.release(Key.ctrl)
+        
+        
+    def transcribe_recording(self, e):
 
-if os.path.exists(pipe_cache_file):
-    pipe = joblib.load(pipe_cache_file)
-    
-else:
-    # If not in cache, create the pipeline and save to cache
-    pipe = pipeline(
-        "automatic-speech-recognition",
-        model="openai/whisper-large-v3",
-        torch_dtype=torch.float16,
-        device="cuda:0" if torch.cuda.is_available() else "cpu",
-        model_kwargs={"attn_implementation": "flash_attention_2"} if is_flash_attn_2_available() else {"attn_implementation": "sdpa"},
-    )
-    joblib.dump(pipe, pipe_cache_file)
-
-
-# Adjust channels based on your audio input setup
-rec = Recorder(channels=1)
-recording = False
-keyboard = Controller()
-print("Ready.")
-
-def paste_transcription(transcript_text):
-    print(transcript_text['text'])
-    transcript_text = transcript_text['text'].replace("New paragraph.", "\n\n")
-    transcript_text = transcript_text.strip()
-    
-    pyperclip.copy(transcript_text)
-    keyboard.press(Key.ctrl)
-    keyboard.press('v')
-    keyboard.release('v')
-    keyboard.release(Key.ctrl)
-
-def transcribe_recording(e):
-    global recording
-
-    if e.event_type == KEY_DOWN and e.name == 'right ctrl' and not recording:
-            recording = True
-            print("Recording started")
-            try:    
-                rec.start()
-                # time.sleep(2)
+        if e.event_type == KEY_DOWN and e.name == self.hook_key and not self.recording:
+            self.recording = True
+            logger.info("Recording started")
+            try:
+                self.rec.start()
+                # if self.model_type == "Faster-Whisper":
+                    # Splitting the data into chunks, running inference while speaking
+                    # audio_array = np.frombuffer(
+                    #         b''.join(self.rec._frames),
+                    #         dtype=np.int8
+                    #         )
+                    
+                    # # Normalize the array to a [-1, 1] range
+                    # audio_array = audio_array.astype(np.float32) / \
+                    #     INT16_MAX_ABS_VALUE
+                        
+                    # model = get_model(self.model_type, self.model_size)
+                    # segments, info = model.transcribe('output.wav', beam_size=5)
+                    # for segment in segments:
+                    #     logger.info(segment.text)
+                    #     self.paste_transcription(segment.text)Thank you.Thank you.Are you listening to what I am saying?Is the audio file changing?
+                        
             except Exception as e:
-                recording = False
-                print("Invalid device configuration.", e)
-                time.sleep(1)  # Brief pause before rebinding
-                hook_key('right ctrl', transcribe_recording)  # Rebind for retry
-            
-
-    elif e.event_type == KEY_UP and e.name == 'right ctrl' and recording:
-        recording = False
-        rec.stop()
-        rec.save("output.wav")
-        print("Recording stopped")
-        
-        # Get length of audio data in seconds
-        with wave.open("output.wav", "rb") as wav_file:
-            frames = wav_file.getnframes()
-            rate = wav_file.getframerate()
-            length = frames / rate
-            
-            if length < 0.2:
-                print("Audio data is less than 0.2s long. Please make a longer recording.")
-                time.sleep(2)  # Brief pause before rebinding
-                hook_key('right ctrl', transcribe_recording)  # Rebind for retry
+                self.recording = False
+                logger.exception("Invalid device configuration.", e)
                 
-            else:
-                outputs = pipe(
-                    "output.wav",
-                    chunk_length_s=30,
-                    batch_size=24,
-                    return_timestamps=True,
-                )
                 
-                paste_transcription(outputs)
-                time.sleep(2)
-        
-# Set up key bindings
-hook_key('right ctrl', transcribe_recording)
+        elif e.event_type == KEY_UP and e.name == self.hook_key and self.recording:
+            self.recording = False
+            self.rec.stop()
+            self.rec.save("output.wav")
+            logger.info("Recording stopped")
+                    
+            # Get length of audio data in seconds
+            with wave.open("output.wav", "rb") as wav_file:
+                frames = wav_file.getnframes()
+                rate = wav_file.getframerate()
+                length = frames / rate
 
-# Keep the program running until Ctrl+Q is pressed
-wait('ctrl+q')
+                if length < 0.5:
+                    logger.warning("Audio data is less than 0.5s long. Please make a longer recording.")
+                    
+                
+                elif not has_speech("output.wav"):
+                    logger.warning("No speech detected during the recording.")
+                    
+                    
+                elif self.model_type == "Insanely-Fast-Whisper":
+                    model = get_model(model_type=self.model_type, model_size=self.model_size)
+                    outputs = model(
+                        "output.wav",
+                        chunk_length_s=30,
+                        batch_size=24,
+                        return_timestamps=True,
+                    )
+                    self.paste_transcription(outputs)
+                
+                else:
+                    model = get_model(self.model_type, self.model_size)
+                    segments, info = model.transcribe('output.wav', beam_size=5)
+                    for segment in segments:
+                        logger.info(segment.text)
+                        self.paste_transcription(segment.text)
+                        
+if __name__ == "__main__":
+    att = AudioToTextRecorder()
+    while True:
+        att.capture_keys()
